26def2296232c837558f1ec77e0178651040c0ac
walid gamal amin <walidgamalamin45@gmail.com>
2025-07-25 01:03:20 +0300

    waled change

--- The_gate/Models/ErrorViewModel.cs
+++ The_gate/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace The_gate.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        //hiiiiii
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
